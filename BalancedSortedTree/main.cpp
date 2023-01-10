e855741795d935a99c5abd53fd4e26ae36f4bf41
AITRAIS Abderrahman <90871152+AbdoAitrais@users.noreply.github.com>
2023-01-10 20:04:26 +0000

    Add files via upload

--- BalancedSortedTree/main.cpp
+++ BalancedSortedTree/main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include "Classes/BSTree.cpp"
+
 
 int main() {
     srand(time(nullptr));
     BSTree<int> bst;
-    int value = 10005;
+    int value = 102005;
+    clock_t start, end;
 
-    for (int i = 0; i < 100000000; ++i) {
+    for (int i = 0; i < 1000000; ++i) {
         bst.insert(i);
     }
-    float start = clock();
-    bool exist = bst.find(value);
-    float end = clock();
 
+    start = clock();
+    bool exist = bst.find(value);
+    end = clock();
     if (exist)
         cout << value << " exist" << endl;
 
     else
         cout << value << " doesn't exist" << endl;
 
-    cout << "Search time : " << (end - start)/CLOCKS_PER_SEC << endl;
+    // Calculating total time taken by the program.
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cout << "Time taken by program is : " << fixed
+         << time_taken << setprecision(5);
+    cout << " sec " << endl;
 
     cout << endl;
 
